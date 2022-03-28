c2b8103f5e337d00a2723cc418f63766a348cdb2
SUNIL KUMAR <SUNIL KUMAR@Lenovo-PC>
2022-03-28 16:07:22 +0530

    new update

---
+++ src/test/java/com/crm/vtiger/demo/CreateLeadTest.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.demo;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadTest {
+
+	@Test
+	public void create() {
+		System.out.println("sucessfully lead created");
+	}
+}
